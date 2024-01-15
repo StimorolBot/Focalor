1edc344ac67baa82f52a6a9a7b2d6a48c64cf7ef
Maxim <maximbotpy@gmail.com>
2024-01-15 20:07:59 +0300

    переделал функцию проверки пользователя

--- Back/src/app/admin_panel/admin_operation.py
+++ Back/src/app/admin_panel/admin_operation.py
@@ -1,43 +1,28 @@
 import uuid
 
-from fastapi import APIRouter, Depends
-
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, delete
+
+from fastapi import Depends, status
+from fastapi_users import FastAPIUsers
+from fastapi.responses import RedirectResponse
+
+from src.config import templates
 from src.database import get_async_session
 from src.app.authentication.models import User
-
-from src.app.authentication.user_manager import get_user_manager
-from fastapi_users import FastAPIUsers
 from src.app.authentication.cookie import auth_backend
+from src.app.authentication.user_manager import get_user_manager
 
 fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend], )
 current_user = fastapi_users.current_user()
 
-router = APIRouter(prefix="/admin_panel", tags=['admin', ])
 
-
-async def select_user(offset: int = 1, session: AsyncSession = Depends(get_async_session)):
-    match offset:
-        case 1:
-            query = select(User).limit(2)
-        case 2:
-            query = select(User).limit(2).offset(offset)
-        case _:
-            offset = offset + (offset - 2)
-            query = select(User).limit(2).offset(offset)
-    query_execute = await session.execute(query)
-    return query_execute.mappings().all()
-
-
-async def check_current_user(user=Depends(current_user)):
+async def check_admin(template: templates.TemplateResponse, user: User):
     try:
         if user.user_role == 0 and user.username == "admin":
-            return "admin"
-        else:
-            return "user"
+            return template
     except AttributeError:
-        return "unauthorized"
+        return RedirectResponse("/error", status_code=status.HTTP_401_UNAUTHORIZED)
 
 
 async def remove_user(user_id: uuid.UUID, session: AsyncSession = Depends(get_async_session)):
