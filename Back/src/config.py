8ba31239c4619c9154f447aadcfef9095a04df91
Maxim <maximbotpy@gmail.com>
2024-01-17 15:01:23 +0300

    инициализировал celery, подключил токен для отправки сообщений на почту

--- Back/src/config.py
+++ Back/src/config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from fastapi.templating import Jinja2Templates
+from celery import Celery
 
 load_dotenv()
 
@@ -16,8 +17,9 @@ DB_USER_TOKEN = os.environ.get("DB_USER_TOKEN")
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 templates = Jinja2Templates(directory="../front/html/")
+celery = Celery('send_email_task', broker='redis://localhost:6379')
 
-SMTP_HOST = ""
-SMTP_PORT = ""
-EMAIL = ""
-PASSWORD = ""
+SMTP_TOKEN = os.environ.get("SMTP_TOKEN")
+SMTP_PORT = os.environ.get("SMTP_PORT")
+SMTP_HOST = os.environ.get("SMTP_HOST")
+SMTP_EMAIL = os.environ.get("SMTP_EMAIL")
