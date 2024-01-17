292352d5f015ad3e2710077764b2a10a8a995e76
Maxim <maximbotpy@gmail.com>
2024-01-17 15:02:32 +0300

    запустил редис при запуске сервера

--- Back/src/main.py
+++ Back/src/main.py
@@ -1,22 +1,24 @@
 from fastapi import FastAPI, Request
+from fastapi_cache import FastAPICache
 from fastapi.staticfiles import StaticFiles
+from fastapi_pagination import add_pagination
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_cache.backends.redis import RedisBackend
 
 from starlette.exceptions import HTTPException
 
-from pages.router import router_page, router_authentication, router_error
 from pages.router_admin import router_admin
+from pages.router import router_page, router_authentication, router_error
+
+from src.config import templates
 
 from src.app.authentication.user_manager import get_user_manager
 from src.app.authentication.cookie import auth_backend
 from src.app.authentication.schemas import UserRead, UserCreate
 from src.app.authentication.models import User
-from src.config import templates
-
-from fastapi_pagination import add_pagination
-
 from src.app.authentication.fastapi_users_custom import FastAPIUsers
-from src.app.authentication.user_operation import router as router_user_operation
+
+from redis import asyncio as aioredis
 
 app = FastAPI(title="main_app")
 
@@ -28,7 +30,7 @@ app.include_router(router_page)
 app.include_router(router_admin)
 app.include_router(router_error)
 app.include_router(router_authentication)
-app.include_router(router_user_operation)
+# app.include_router(router_user_operation)
 
 fastapi_users = FastAPIUsers[User, int](get_user_manager, [auth_backend], )
 
@@ -46,6 +48,10 @@ origins = [
 
 # http://127.0.0.1:8000/docs документация
 # uvicorn src.main:app --reload - запуск сервера
+# celery -A src.app.authentication.background_tasks.send_email:celery worker --loglevel=INFO --pool=solo
+# - запуск celery (перезагружать после изменения кода) (прописывать путь туда, где вещается task)
+# celery -A src.config:celery flower - запуск веб интерфейса
+# http://localhost:5555/ интерфейс
 
 
 app.add_middleware(
@@ -67,3 +73,9 @@ async def exception_handler(request: Request, exc: HTTPException):
         return templates.TemplateResponse("error_page.html", {"request": request})
     else:
         print(f"\n*************Неизвестный_статс_код{exc.status_code}*************\n")
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
