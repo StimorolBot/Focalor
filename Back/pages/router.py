dcf4f7f26f9c337b7817c2af13ec8f36fc503f84
Maxim <maximbotpy@gmail.com>
2024-01-18 19:11:14 +0300

    добавил эндпоинт для верификации

--- Back/pages/router.py
+++ Back/pages/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Request
+
 from src.config import templates
+from src.app.authentication.background_tasks.verified_token import verified
 
 router_page = APIRouter(tags=["pages"])
 router_authentication = APIRouter(tags=["authentication"])
@@ -19,6 +21,18 @@ async def get_login_page(request: Request):
 @router_authentication.get("/register")
 async def get_register_page(request: Request):
     return templates.TemplateResponse("authentication/register.html", {"request": request})
+
+
+@router_authentication.get("/is_verified/{token}")
+async def get_verified_page(request: Request):
+    verified.token_request = request["path"].split("/")[2]
+
+    if verified.verified():
+        return templates.TemplateResponse("authentication/verified.html", {"request": request, "msg": "Успешное подтверждение почты !"})
+
+    return templates.TemplateResponse("authentication/verified.html",
+                                      {"request": request,
+                                       "msg": "Не удалось подтвердить почту, пожалуйста, попробуйте еще раз !"})
 
 
 @router_error.get("/error")
