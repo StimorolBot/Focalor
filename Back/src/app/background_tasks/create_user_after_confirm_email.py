6cca5974e4b9e29579cf82c5d533e323d1b94980
maxim <maximbotpy@gmail.com>
2024-01-25 23:39:30 +0300

    создал CreateUser который создает пользователя после подтверждения почты

---
+++ Back/src/app/background_tasks/create_user_after_confirm_email.py
@@ -0,0 +1,35 @@
+from fastapi import HTTPException, status, Request
+
+from fastapi_users import exceptions, schemas
+from fastapi_users.router.common import ErrorCode
+
+
+class CreateUser:
+    user_manager = None
+    request: Request
+    user_create: str
+    user_schema: str
+
+    token_request = None
+    token_render = None
+
+    async def verified_token(self):
+        if self.token_request is None or self.token_render is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={"code": "INVALID_TOKEN",
+                        "data": "Токен невалиден"})
+
+        elif self.token_request == self.token_render:
+            return True
+
+    async def create(self):
+        try:
+            created_user = await self.user_manager.create(self.user_create, safe=True, request=self.request)
+        except exceptions.UserAlreadyExists:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={"code": ErrorCode.REGISTER_USER_ALREADY_EXISTS,
+                        "data": "Пользователь с таким именем/почтой уже существует"})
+
+        return schemas.model_validate(self.user_schema, created_user)
