f15d176d384c0103b1ac0780ab7b2314473514ad
maxim <maximbotpy@gmail.com>
2024-02-02 17:29:17 +0300

    добавил вызов функции получения списка картинок

--- Back/src/router/router_user.py
+++ Back/src/router/router_user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 
 from src.config import templates
+from src.help_finc.get_list_img import get_list_img
 from src.app.background_tasks.create_user_after_confirm_email import CreateUser
 
 user = CreateUser()
@@ -9,7 +10,11 @@ router_user = APIRouter(tags=["pages"])
 
 @router_user.get("/")
 async def get_home_page(request: Request):
-    return templates.TemplateResponse("main.html", {"request": request})
+    list_img_slider = await get_list_img(path="../Front/img/slider")
+    list_img_bg = await get_list_img(path="../Front/img/bg")
+    return templates.TemplateResponse("main.html", {"request": request, "title": "Focalors",
+                                                    "list_img_slider": list_img_slider, "list_img_bg": list_img_bg,
+                                                    "indicators": len(list_img_bg)})
 
 
 @router_user.get("/verified/{token}")
