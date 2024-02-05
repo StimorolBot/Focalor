66a0f81347bc704bd6daa99c211e71488bd0ebb2
maxim <maximbotpy@gmail.com>
2024-02-05 20:58:26 +0300

    удалил еще один файл

--- Back/src/help_finc/get_list_img.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_list_img(path: str) -> list:
-    if os.path.exists(path) and os.path.isdir(path):
-        return os.listdir(path)
-    else:
-        raise FileNotFoundError("[!] Директория не найдена")
-
