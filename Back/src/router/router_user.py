7b1ef0bda5e6738b7f9798c86a8efd84bbc73232
maxim <maximbotpy@gmail.com>
2024-02-11 22:34:12 +0300

    добавил роутер с подпиской рассылки

--- Back/src/router/router_user.py
+++ Back/src/router/router_user.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Request
+from fastapi_users import exceptions
+from fastapi import APIRouter, Request, Depends
 
 from src.config import templates
-from src.help_finc.get_list_img import get_list_img
-from src.app.background_tasks.create_user_after_confirm_email import CreateUser
+from src.help_func.get_list_img import get_list_img
+from src.app.authentication.operations.user_operation import user
+from src.app.authentication.user_manager import get_user_manager, UserManager
 
-user = CreateUser()
-router_user = APIRouter(tags=["pages"])
+router_user = APIRouter(tags=["user"])
 
 
 @router_user.get("/")
@@ -20,7 +21,17 @@ async def get_home_page(request: Request):
 @router_user.get("/verified/{token}")
 async def get_verified_page(request: Request):
     user.token_request = request["path"].split("/")[2]
-
-    if user.verified_token():
+    if await user.verified_token() is True:
         await user.create()
-        return templates.TemplateResponse("authentication/verified.html", {"request": request, "msg": "Успешное подтверждение почты !"})
+        return templates.TemplateResponse("authentication/verified.html", {"request": request, "title": "Verified",
+                                                                           "msg": "Успешное подтверждение почты !"})
+
+
+@router_user.post("/subscribe")
+async def subscribe_newsletter(request: Request, user_manager: UserManager = Depends(get_user_manager)):
+    data = await request.json()
+    try:
+        await user_manager.get_by_email(data["email"].replace("%", "@"))
+    except exceptions.UserNotExists:
+        print(f"[!] Почта {data['email'].replace('%', '@')} не найдена")
+    return await request.json()
