dd0f039308e3137ca630a2986ccd096b91a8fd27
maxim <maximbotpy@gmail.com>
2024-02-11 19:02:07 +0300

    удалил файлы

---
+++ Back/src/app/authentication/models/__init__.py
@@ -0,0 +1,9 @@
+from src.app.authentication.models.base import Base
+from src.app.authentication.models.user import User
+from src.app.authentication.models.role import Role
+
+__all__ = (
+    "Base",
+    "User",
+    "Role"
+)
