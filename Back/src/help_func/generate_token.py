84ddf395c9ad36a32242bd785ba15a577e25278f
maxim <maximbotpy@gmail.com>
2024-02-11 14:57:19 +0300

    изменил путь импорта файлов

--- Back/src/help_func/generate_token.py
+++ Back/src/help_func/generate_token.py
@@ -1,21 +1,21 @@
 import string
 import secrets
+from typing import Optional
 from fastapi import Request
 from datetime import datetime, timedelta
 
 from src.config import LIFETIME
-from src.help_func.user_states import UserStates
-
+from src.app.authentication.operations.states import UserStates
 
 
 def render_token(token_len: int, lifetime_seconds: int) -> dict:
     letters_and_digits = string.ascii_letters + string.digits
     token = ''.join(secrets.choice(letters_and_digits) for _ in range(token_len))
-    ttl = datetime.utcnow() + timedelta(seconds=lifetime_seconds)
+    ttl = datetime.utcnow() + timedelta(seconds=int(lifetime_seconds))
     return {"token": token, "ttl": ttl}
 
 
-def get_token(states: UserStates, request: Request) -> dict:
+def get_token(states: UserStates, request: Optional[Request] = None) -> dict:
     match states.value:
         case "email_confirm":
             data = render_token(token_len=64, lifetime_seconds=LIFETIME)
