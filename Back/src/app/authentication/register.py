537d056ed69bbdf98acfec82f33698e5e32a63d2
maxim <maximbotpy@gmail.com>
2024-02-15 22:16:44 +0300

    изменил пти импортов, перенес вызов исключения при одинаковой почте

--- Back/src/app/authentication/register.py
+++ Back/src/app/authentication/register.py
@@ -4,10 +4,13 @@ from fastapi_users import models, schemas
 from fastapi_users.router.common import ErrorCode, ErrorModel
 from fastapi_users.manager import BaseUserManager, UserManagerDependency
 
+from fastapi.exceptions import HTTPException
 from fastapi import APIRouter, Depends, Request, status
 
-from src.app.background_tasks.send_email import send_email
-from src.app.background_tasks.create_user_after_confirm_email import CreateUser
+from src.help_func.generate_token import get_token
+from src.app.authentication.operations.states import UserStates
+from src.background_tasks.send_email import send_email
+from src.app.authentication.operations.user_operation import user
 
 
 def get_register_user(get_user_manager: UserManagerDependency[models.UP, models.ID], user_schema: Type[schemas.U],
@@ -45,11 +48,25 @@ def get_register_user(get_user_manager: UserManagerDependency[models.UP, models.
                  )
     async def register(request: Request, user_create: user_schema = Depends(user_create_schema),  # type: ignore
                        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager), ):
-        send_email(action="email_confirm", request=request)
+        await user_manager.validate_password(user_create.password, user_create)
 
-        CreateUser.user_manager = user_manager
-        CreateUser.request = request
-        CreateUser.user_create = user_create
-        CreateUser.user_schema = user_schema
+        existing_user = await user_manager.user_db.get_by_email(user_create.email)
+
+        if existing_user is not None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={"code": ErrorCode.REGISTER_USER_ALREADY_EXISTS,
+                        "data": "Пользователь с таким именем/почтой уже существует"})
+
+        token = get_token(states=UserStates.EMAIL_CONFIRM, request=request)
+        send_email(state=UserStates.EMAIL_CONFIRM, token=token["token"])
+
+        user.ttl = token["ttl"]
+        user.token = token["token"].split("/")[2]
+        user.user_email = user_create.email
+        user.user_manager = user_manager
+        user.user_create = user_create
+        user.user_schema = user_schema
+        user.request = request
 
     return router
