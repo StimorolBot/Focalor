df4c0c20a7f7ac88310ec32865fa00b97eb8d7ee
maxim <maximbotpy@gmail.com>
2024-02-15 22:09:53 +0300

    изменил пути импортов

--- Back/src/app/authentication/models/news_letter.py
+++ Back/src/app/authentication/models/news_letter.py
@@ -1,22 +1,22 @@
 from uuid import UUID
+from .base import Base
 from typing import TYPE_CHECKING
 
-from src.models.base import Base
-from src.models.custom_type import intpk
-
-from sqlalchemy import TIMESTAMP, Boolean, ForeignKey
+from sqlalchemy import TIMESTAMP, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
-    from src.app.authentication.models import User
+    from .user import User
+
+from .custom_type import intpk
 
 
 class NewsLetter(Base):
     __tablename__ = "News_Letter_Table"
 
     id: Mapped[intpk]
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("User_Table.id"))
-    user: Mapped["User"] = relationship(back_populates="is_subscription")
-    email: Mapped[str] = mapped_column(unique=True, default=None, server_default="", nullable=True)
-    subscription_date: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, default="", server_default="")
-    is_subscription: Mapped[bool] = mapped_column(Boolean, default=False, server_default="")
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("User_Table.id"), unique=True)
+    email: Mapped[str] = mapped_column(unique=True, default=None, server_default="")
+    subscription_date: Mapped[TIMESTAMP | None] = mapped_column(TIMESTAMP, default=None, server_default=None)
+    is_subscription: Mapped[bool] = mapped_column(default=False, server_default=None)
+    user: Mapped["User"] = relationship("User", back_populates="user_subscription", uselist=False)
