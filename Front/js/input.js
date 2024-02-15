de66c890512ccfbf046e01a398c5fd28b93ec042
maxim <maximbotpy@gmail.com>
2024-02-15 21:50:43 +0300

    навесил событие на кнопку показа пароля

--- Front/js/input.js
+++ Front/js/input.js
@@ -1,17 +1,19 @@
-let input_password = document.querySelector('.login__password-input');
-let show_password_btn = document.querySelector('.fa-eye');
+let input_password = document.getElementsByName("password");
+let show_password_btn = document.querySelectorAll('.fa-eye');
 
+show_password_btn.forEach( function(element, index) {
+	element.addEventListener('click', function(event){
+		
+		if(input_password[index].type === 'password'){
+			input_password[index].setAttribute('type', 'text');
+			show_password_btn[index].classList.remove('fa-eye');
+			show_password_btn[index].classList.add('fa-eye-slash');
+		}
 
-function show_password(){
-	if(input_password.type === 'password'){
-		input_password.setAttribute('type', 'text');
-		show_password_btn.classList.remove('fa-eye');
-		show_password_btn.classList.add('fa-eye-slash');
-	}
-	
-	else{
-		input_password.setAttribute('type', 'password');
-		show_password_btn.classList.remove('fa-eye-slash');
-		show_password_btn.classList.add('fa-eye'); 
-    }
-}
+		else{
+			input_password[index].setAttribute('type', 'password');
+			show_password_btn[index].classList.remove('fa-eye-slash');
+			show_password_btn[index].classList.add('fa-eye'); 
+		}
+	});
+})
