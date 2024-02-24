322e912c5808d0e438b73d02594ada391611c119
maxim <maximbotpy@gmail.com>
2024-02-25 01:13:44 +0300

    убрал лишнее

--- Front/js/comment/send_comment.js
+++ Front/js/comment/send_comment.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".footer__subscription_form").on("submit", function(event){
+  $(".comment__form").on("submit", function(event){
 		event.preventDefault();
 		var data =  $(this).serialize();
 
@@ -10,41 +10,15 @@ $(document).ready(function(){
       contentType: "application/json",
       
       data: JSON.stringify({
-        email: data.split("=")[1]
+        comment: data.split("=")[1]
       }),
-    });
-	});
-
-  $(".popup__btn-send").on("click", function(event){
-    event.preventDefault();
-    var user_email = $("#email_reset_password").val();
-    var btn = $(this);
-
-    if ((user_email.length >=8) && (user_email.includes("@")) && (user_email.includes("."))){
-      btn.attr('disabled', true);
-
-      setTimeout(function() {
-        btn.attr('disabled', false);
-      }, 60000);
-
-      $.ajax({
-        url: "reset-password/code",
-        type: "POST",
-        dataType: "json",
-        contentType: "application/json",
-        
-        data: JSON.stringify({
-          email: user_email
-        }),
-            
-        success: function(result){
+      success: function(result){
           console.log("success", result);
         },
             
         error: function(data){
           console.log("error", data);
         }
-      });
-    }
-  });
+    });
+	});
 });	
