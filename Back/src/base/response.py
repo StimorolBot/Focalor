e89eaf85860a3495e8dd8cf0a8aae47f3df35da6
maxim <maximbotpy@gmail.com>
2024-02-25 01:24:06 +0300

    исправил опечатки

--- Back/src/base/response.py
+++ Back/src/base/response.py
@@ -1,9 +1,5 @@
-from typing import Optional, Type, Dict, Any, List, Union
 from pydantic import BaseModel, ConfigDict
-from fastapi.templating import Jinja2Templates
 
-
-# request: Optional[Type[Request]] = None
 
 class Response(BaseModel):
     status_code: int
