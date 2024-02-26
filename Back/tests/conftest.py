fb3cffb51f9cf7b4c23e1a083987ad54864f7d6b
maxim <maximbotpy@gmail.com>
2024-02-26 23:57:29 +0300

    базовая конфигурация для тест файлов

---
+++ Back/tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+import asyncio
+
+from httpx import AsyncClient
+from typing import AsyncGenerator
+
+from sqlalchemy.pool import NullPool
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from tests.setting import setting_test
+
+from src.main import app
+from src.base.declarative_base import Base
+from src.database import get_async_session
+
+engine_test = create_async_engine(setting_test.DB_TEST_URL, poolclass=NullPool)
+async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+Base.metadata.bind = engine_test
+
+
+async def test_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[get_async_session] = test_get_async_session
+
+
+@pytest.fixture(autouse=True, scope='session')
+async def prepare_database():
+    assert setting_test.MODE == "TEST"
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine_test.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def ac() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
