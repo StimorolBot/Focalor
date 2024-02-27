4091dfa4883665b93cd33e5f1b343c233d653a56
maxim <maximbotpy@gmail.com>
2024-02-27 22:08:33 +0300

    переделал валидацию пароля

--- Back/src/app/authentication/schemas/user_auth.py
+++ Back/src/app/authentication/schemas/user_auth.py
@@ -1,3 +1,4 @@
+import re
 import uuid
 from typing import Optional
 from datetime import datetime
@@ -6,10 +7,39 @@ from typing_extensions import Annotated
 from fastapi_users import schemas
 from fastapi import HTTPException, status
 from fastapi.param_functions import Form
-from fastapi_users.router.common import ErrorCode
 
 from pydantic import EmailStr, BaseModel, ConfigDict
 from pydantic.functional_validators import model_validator
+
+from src.base.response import Response as ResponseSchemas
+
+
+class CheckPassword(BaseModel):
+    password: Annotated[str, Form(min_length=8, max_length=20)]
+    password_confirm: Annotated[str, Form(min_length=8, max_length=20)]
+
+    @model_validator(mode='after')
+    def check_password(self) -> str:
+        psd = self.password
+        psd_conf = self.password_confirm
+
+        forbidden_symbols = """ "@!#$%^&*'_-+=/|\.,:;()[]{}"""
+
+        match psd:
+            case psd if psd != psd_conf:
+                detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST, data="Пароли не совпадают").model_dump()
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+            case psd if [symbol for symbol in psd if symbol in forbidden_symbols]:
+                detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST,
+                                         data=f"Пароль не должно содержать запрещенных символов:{forbidden_symbols}").model_dump()
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+            case psd if re.findall("[^0-9a-zA-Z_]", psd):
+                detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST, data="Пароль не должен включать кириллицу").model_dump()
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+        return psd
 
 
 # будет отображаться при успешном создании пользователя
@@ -25,50 +55,23 @@ class UserRead(schemas.BaseUser[uuid.UUID]):
     model_config = ConfigDict(from_attributes=True)
 
 
-# чтобы не было ошибки добавил в BaseUserCreate username
-class UserCreate(schemas.BaseUserCreate):
-    def __init__(self, email: Annotated[EmailStr, Form()],
-                 username: Annotated[str, Form()],
-                 password: Annotated[str, Form()],
-                 is_active: Optional[bool] = True,
-                 is_superuser: Optional[bool] = False,
-                 is_verified: Optional[bool] = False):
-        super().__init__(
-            email=email,
-            username=username,
-            password=password,
-            is_active=is_active,
-            is_superuser=is_superuser,
-            is_verified=is_verified)
+class UserCreate(schemas.BaseUserCreate, CheckPassword):
+    email: Annotated[EmailStr, Form(min_length=8, max_length=40)]
+    username: Annotated[str, Form(min_length=8, max_length=40)]
+    password: Annotated[str, Form(min_length=8, max_length=20)]
+    password_confirm: Annotated[str, Form(min_length=8, max_length=20)]
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
 
     model_config = ConfigDict(from_attributes=True)
-
-    @model_validator(mode='after')
-    def check_password(self):
-        psd = self.password
-        forbidden_symbols = " @!#$%^&*()_-+=/.,:;[]{}"
-        if len(psd) <= 8:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail={"code": ErrorCode.REGISTER_INVALID_PASSWORD,
-                        "data": "Длинна пароля должна быть больше 8 символов"})
-
-        elif [symbol for symbol in psd if symbol in forbidden_symbols]:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail={"code": ErrorCode.REGISTER_INVALID_PASSWORD,
-                        "data": f"Пароль не должно содержать запрещенных символов:{forbidden_symbols}"})
-        return psd
-
 
 class UserUpdate(schemas.BaseUserUpdate):
     ...
 
 
-class UserResetPassword(BaseModel):
+class UserResetPassword(CheckPassword):
     email: Annotated[EmailStr, Form(min_length=8, max_length=40)]
     token: Annotated[str, Form(max_length=6)]
-    password: Annotated[str, Form(min_length=8, max_length=20)]
-    password_repeat: Annotated[str, Form(min_length=8, max_length=20)]
 
     model_config = ConfigDict(from_attributes=True)
