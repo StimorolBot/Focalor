d1a3f8fcb792ea50b88d9dd391ba4507e0d8b6b6
maxim <maximbotpy@gmail.com>
2024-03-02 00:07:57 +0300

    убрал ttl

--- Back/src/help_func/generate_token.py
+++ Back/src/help_func/generate_token.py
@@ -2,26 +2,22 @@ import string
 import secrets
 from typing import Optional
 from fastapi import Request
-from datetime import datetime, timedelta
-
-from src.config import LIFETIME
 from src.app.authentication.operations.states import UserStates
 
 
-def render_token(token_len: int, lifetime_seconds: int) -> dict:
+def render_token(token_len: int = 64) -> str:
     letters_and_digits = string.ascii_letters + string.digits
     token = ''.join(secrets.choice(letters_and_digits) for _ in range(token_len))
-    ttl = datetime.utcnow() + timedelta(seconds=int(lifetime_seconds))
-    return {"token": token, "ttl": ttl}
+    return token
 
 
-def get_token(states: UserStates, request: Optional[Request] = None) -> dict:
+def get_token(states: UserStates, request: Optional[Request] = None) -> list | str:
     match states.value:
         case "email_confirm":
-            data = render_token(token_len=64, lifetime_seconds=LIFETIME)
-            data["token"] = f"{request.url.hostname}:{request.url.port}/verified/{data['token']}"
-            return data
+            token = render_token()
+            url = f"{request.url.hostname}:{request.url.port}/verified/{token}"
+            return [token, url]
         case "reset_password":
-            return render_token(token_len=6, lifetime_seconds=LIFETIME)
+            return render_token(token_len=6)
         case _:
             raise "[!] неизвестное состояние"
