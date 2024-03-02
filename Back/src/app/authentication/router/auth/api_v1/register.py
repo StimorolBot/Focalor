f602656a3177e563417566657d26b40b77170f92
maxim <maximbotpy@gmail.com>
2024-03-03 00:20:16 +0300

    перенес в отдельную папку

---
+++ Back/src/app/authentication/router/auth/api_v1/register.py
@@ -0,0 +1,75 @@
+import json
+from typing import Type
+
+from fastapi.exceptions import HTTPException
+from fastapi import APIRouter, Depends, Request, status
+
+from fastapi_users import schemas
+from fastapi_users.openapi import OpenAPIResponseType
+from fastapi_users.router.common import ErrorCode, ErrorModel
+
+from core.config import redis
+from core.config import setting
+from core.enum.email_states import EmailStates
+from core.schemas.response import Response as ResponseSchemas
+
+from src.help_func.generate_token import get_token
+from src.app.authentication.user_manager import get_user_manager, UserManager
+
+
+def get_register_user(user_create_schema: Type[schemas.UC], ) -> APIRouter:
+    router = APIRouter()
+
+    register_response: OpenAPIResponseType = {
+        status.HTTP_400_BAD_REQUEST: {
+            "model": ErrorModel,
+            "content": {
+                "application/json": {
+                    "examples": {
+                        ErrorCode.REGISTER_USER_ALREADY_EXISTS: {
+                            "summary": "Пользователь с таким адресом электронной почты уже существует",
+                            "value": {
+                                "detail": ErrorCode.REGISTER_USER_ALREADY_EXISTS
+                            },
+                        },
+                        ErrorCode.REGISTER_INVALID_PASSWORD: {
+                            "summary": "Проверка пароля не удалась.",
+                            "value": {
+                                "detail": {
+                                    "code": ErrorCode.REGISTER_INVALID_PASSWORD,
+                                    "reason": "Пароль должен состоять минимум из 8 символов",
+                                }
+                            },
+                        },
+                    }
+                }
+            },
+        },
+    }
+
+    @router.post("/register", name="register:register", responses=register_response)
+    async def register(request: Request, user_create: user_create_schema = Depends(user_create_schema),
+                       user_manager: UserManager = Depends(get_user_manager), ) -> ResponseSchemas:
+        await user_manager.validate_password(user_create.password, user_create)
+        existing_user = await user_manager.user_db.get_by_email(user_create.email)
+
+        if existing_user is not None:
+            detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST, data="Пользователь с таким почтой уже существует")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+        token, url = get_token(states=EmailStates.EMAIL_CONFIRM, request=request)
+
+        user_dict = {
+            "email": user_create.email,
+            "username": user_create.username,
+            "password": user_create.password,
+            "password_confirm": user_create.password_confirm
+        }
+
+        user_str = json.dumps(user_dict)
+        await redis.set(name=token, value=user_str, ex=setting.EX)
+
+        await user_manager.send_email_confirm(user_create.email, request, url)
+        return ResponseSchemas(status_code=status.HTTP_200_OK, data="Для завершения регистрации проверьте свой почтовый ящик")
+
+    return router
