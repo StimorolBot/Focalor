87a76c975daf10c282dcc3a4d76b896eef601638
maxim <maximbotpy@gmail.com>
2024-03-03 00:20:52 +0300

    перенес проверку полей в отдельынй файл

--- Back/src/app/authentication/schemas/user_auth.py
+++ Back/src/app/authentication/schemas/user_auth.py
@@ -1,45 +1,13 @@
-import re
 import uuid
 from typing import Optional
 from datetime import datetime
 from typing_extensions import Annotated
+from pydantic import EmailStr, ConfigDict
 
 from fastapi_users import schemas
-from fastapi import HTTPException, status
 from fastapi.param_functions import Form
 
-from pydantic import EmailStr, BaseModel, ConfigDict
-from pydantic.functional_validators import model_validator
-
-from src.base.response import Response as ResponseSchemas
-
-
-class CheckPassword(BaseModel):
-    password: Annotated[str, Form(min_length=8, max_length=20)]
-    password_confirm: Annotated[str, Form(min_length=8, max_length=20)]
-
-    @model_validator(mode='after')
-    def check_password(self) -> str:
-        psd = self.password
-        psd_conf = self.password_confirm
-
-        forbidden_symbols = """ "@!#$%^&*'_-+=/|\.,:;()[]{}"""
-
-        match psd:
-            case psd if psd != psd_conf:
-                detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST, data="Пароли не совпадают").model_dump()
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
-
-            case psd if [symbol for symbol in psd if symbol in forbidden_symbols]:
-                detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST,
-                                         data=f"Пароль не должно содержать запрещенных символов:{forbidden_symbols}").model_dump()
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
-
-            case psd if re.findall("[^0-9a-zA-Z_]", psd):
-                detail = ResponseSchemas(status_code=status.HTTP_400_BAD_REQUEST, data="Пароль не должен включать кириллицу").model_dump()
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
-
-        return psd
+from core.schemas.check_field import CheckPassword, CheckEmail
 
 
 # будет отображаться при успешном создании пользователя
@@ -66,12 +34,12 @@ class UserCreate(schemas.BaseUserCreate, CheckPassword):
 
     model_config = ConfigDict(from_attributes=True)
 
+
 class UserUpdate(schemas.BaseUserUpdate):
     ...
 
 
-class UserResetPassword(CheckPassword):
-    email: Annotated[EmailStr, Form(min_length=8, max_length=40)]
+class UserResetPassword(CheckPassword, CheckEmail):
     token: Annotated[str, Form(max_length=6)]
 
     model_config = ConfigDict(from_attributes=True)
