e51b5da0cc93631920e6782a4188b3524797ed3c
maxim <maximbotpy@gmail.com>
2024-03-03 00:24:21 +0300

    переименовал

---
+++ Front/js/ajax.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+
+  $(".footer__subscription_form").on("submit", function(event){
+		event.preventDefault();
+		var data =  $(this).serialize();
+
+		$.ajax({
+      url: $(this).attr("action"),
+      type: $(this).attr("method"),
+      dataType: "json",
+      contentType: "application/json",
+      
+      data: JSON.stringify({
+        email: data.split("=")[1]
+      }),
+    });
+	});
+
+  $(".popup__btn-send").on("click", function(event){
+    event.preventDefault();
+    var user_email = $("#email_reset_password").val();
+    var btn = $(this);
+
+    btn.attr('disabled', true);
+
+    setTimeout(function() {
+      btn.attr('disabled', false);
+    }, 60000);
+
+    $.ajax({
+      url: "/reset-password/code-confirm",
+      type: "post",
+      dataType: "json",
+      contentType: "application/json",
+        
+      data: JSON.stringify({
+        email: user_email
+      }),
+            
+        success: function(result){
+          console.log("success", result);
+        },
+            
+        error: function(data){
+          console.log("error", data);
+        }
+      });
+  });
+
+
+  $(".popup__btn-next-step").on("click", function(event){
+    event.preventDefault();
+    var data =  $(this).serialize();
+
+    $.ajax({
+      url: "/reset-password",
+      type: "patch",
+      dataType: "json",
+      contentType: "application/json",
+        
+      data: JSON.stringify({
+        data: data
+      }),
+            
+        success: function(result){
+          console.log("success", result);
+        },
+            
+        error: function(data){
+          console.log("error", data);
+        }
+      });
+  });
+});	
