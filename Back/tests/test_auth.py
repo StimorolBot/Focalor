8d98dc235be6a6efa239f810311ffc0b9276fee7
maxim <maximbotpy@gmail.com>
2024-03-03 00:24:01 +0300

    исправил тест

--- Back/tests/test_auth.py
+++ Back/tests/test_auth.py
@@ -1,17 +1,16 @@
-from fastapi import status
 from httpx import AsyncClient
 from tests.conftest import ac
 
 
-# pytest -v -s tests/ - запуск тестов
+# pytest  -vv -s  tests/test_auth.py::TestAuth - запуск тестов
 
 class TestAuth:
 
     async def test_register(self, ac: AsyncClient):
-        user_date = {"email": "maxim@test.reg", "username": "maxim_test", "password": "passwordtest"}
+        user_date = {"password": "passwordtest", "password_confirm": "passwordtest", "email": "email@my.com", "username": "myNameTest"}
         response = await ac.post("/register", data=user_date)
 
         assert response.json() == {
-            "status_code": status.HTTP_200_OK,
-            "detail": "Для завершения регистрации проверьте свой почтовый ящик"
+            "status_code": 200,
+            "data": "Для завершения регистрации проверьте свой почтовый ящик"
         }
