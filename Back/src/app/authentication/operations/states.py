c173885518f9bf8a98f8ef36373ac57b30834860
maxim <maximbotpy@gmail.com>
2024-03-03 00:09:32 +0300

    перенес в core

--- Back/src/app/authentication/operations/states.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class UserStates(Enum):
-    ON_AFTER_REGISTER = "on_after_register"
-    EMAIL_CONFIRM = "email_confirm"
-    RESET_PASSWORD = "reset_password"
-    CREATE_USER = "create_user"
