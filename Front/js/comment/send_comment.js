f03db272cf4cdcc05a2f050455cb863e09164158
maxim <maximbotpy@gmail.com>
2024-03-08 01:06:46 +0300

    добавил добавления коментария при успешном ответе сервера

--- Front/js/comment/send_comment.js
+++ Front/js/comment/send_comment.js
@@ -1,24 +1,26 @@
-$(document).ready(function(){
-  $(".comment__form").on("submit", function(event){
-		event.preventDefault();
-		var data =  $(this).serialize();
+$(document).ready(function () {
+    $(".comment__form").on("submit", function (event) {
+        event.preventDefault();
+        var data = $(this).serialize().split("=")[1];
 
-		$.ajax({
-      url: $(this).attr("action"),
-      type: $(this).attr("method"),
-      dataType: "json",
-      contentType: "application/json",
-      
-      data: JSON.stringify({
-        comment: data.split("=")[1]
-      }),
-      success: function(result){
-          console.log("success", result);
-        },
-            
-        error: function(data){
-          console.log("error", data);
-        }
+        $.ajax({
+            url: $(this).attr("action"),
+            type: $(this).attr("method"),
+            dataType: "json",
+            contentType: "application/json",
+
+            data: JSON.stringify({
+                comment: data
+            }),
+
+            success: function (result) {
+                $(`<div class='comment__user'>${result}</div><br>`).appendTo(".wrapper");
+                $("#comment").val("");
+            },
+
+            error: function (data) {
+                console.log("error", data);
+            }
+        });
     });
-	});
 });	
