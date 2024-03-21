08911412b3c742a5e143170c0096742b74513178
maxim <maximbotpy@gmail.com>
2024-03-21 23:25:48 +0300

    исправил опечатку

--- Back/src/app/authentication/router/auth/api_v1/verified.py
+++ Back/src/app/authentication/router/auth/api_v1/verified.py
@@ -11,7 +11,7 @@ from fastapi_users.router.common import ErrorCode, ErrorModel
 from src.app.authentication.schemas.user_auth import UserCreate
 
 
-def get_verify_user(user_create_schema: UserCreate) -> APIRouter:
+def get_verify_user() -> APIRouter:
     router = APIRouter()
 
     verify_responses: OpenAPIResponseType = {
@@ -21,12 +21,11 @@ def get_verify_user(user_create_schema: UserCreate) -> APIRouter:
                 "application/json": {
                     "examples": {
                         ErrorCode.VERIFY_USER_BAD_TOKEN: {
-                            "summary": "Bad token, not existing user or"
-                                       "not the e-mail currently set for the user.",
+                            "summary": "Неверный токен",
                             "value": {"detail": ErrorCode.VERIFY_USER_BAD_TOKEN},
                         },
                         ErrorCode.VERIFY_USER_ALREADY_VERIFIED: {
-                            "summary": "The user is already verified.",
+                            "summary": "Пользователь уже верифицирован",
                             "value": {
                                 "detail": ErrorCode.VERIFY_USER_ALREADY_VERIFIED
                             },
@@ -45,7 +44,7 @@ def get_verify_user(user_create_schema: UserCreate) -> APIRouter:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Токен невалиден")
 
         user_dict = json.loads(user_str)
-        user_create = user_create_schema(**user_dict)
+        user_create = UserCreate(**user_dict)
         await user_manager.create(user_create=user_create)
 
         return ResponseSchemas(status_code=status.HTTP_201_CREATED, data=f"Пользователь {user_dict['username']} создан")
