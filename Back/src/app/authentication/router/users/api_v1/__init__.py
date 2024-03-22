e2cf3e6c0e2c6fa7e47238d043b7f9fb6cb6b047
maxim <maximbotpy@gmail.com>
2024-03-22 22:21:56 +0300

    перенес файлы

--- Back/src/app/authentication/router/users/api_v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .user import router_user
-from .admin import router_admin
-
-router = APIRouter(tags=["api_v1"])
-
-router.include_router(router_admin)
-router.include_router(router_user)
