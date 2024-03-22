256bd63b75c5a6c7e1bf78267bd825804092fc5e
maxim <maximbotpy@gmail.com>
2024-03-23 00:45:51 +0300

    переименовал функции

--- Back/core/operation/convert.py
+++ Back/core/operation/convert.py
@@ -7,6 +7,6 @@ async def add_to_redis(user_data: dict, name: str):
     await redis.set(name=name, value=user_str, ex=setting.EX)
 
 
-async def get_user_redis(key: str) -> dict:
-    user_str = await redis.get(key)
-    return json.loads(user_str)
+async def get_to_redis(key: str) -> dict:
+    item = await redis.get(key)
+    return json.loads(item)
