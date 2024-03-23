320ac60893636c131717edf61bcb019cb0070488
maxim <maximbotpy@gmail.com>
2024-03-24 00:26:06 +0300

    добавил запрос на получение комментариев

--- Front/js/comment/send_comment.js
+++ Front/js/comment/send_comment.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    let comment = $('.comment__container');
+    let page = 1;
+    comment.scrollTop(comment[0].scrollHeight);
+
     $(".comment__form").on("submit", function (event) {
         event.preventDefault();
-        var data = $(this).serialize().split("=")[1];
+        let data = $(this).serialize().split("=")[1];
 
         $.ajax({
             url: $(this).attr("action"),
@@ -14,7 +18,7 @@ $(document).ready(function () {
             }),
 
             success: function (result) {
-                $(`<div class='comment__user'>${result}</div><br>`).appendTo(".wrapper");
+                $(`<div class='wrapper-msg'><div class='comment comment__user'> ${result}</div></div><br>`).appendTo(".wrapper");
                 $("#comment").val("");
             },
 
@@ -23,4 +27,29 @@ $(document).ready(function () {
             }
         });
     });
-});	
+
+    comment.scroll(function(event){
+        event.preventDefault();
+        if ( $(this).scrollTop() == 0 ){
+
+            $.ajax({
+                url: "/comment",
+                type: "get",
+                dataType: "json",
+                contentType: "application/json",
+
+                data: JSON.stringify({
+                    page: ++page
+                }),
+
+                success: function (result) {
+                    console.log(result)
+                },
+
+                error: function (data) {
+                    console.log("error", data);
+                }
+            });
+        }
+    });
+});
