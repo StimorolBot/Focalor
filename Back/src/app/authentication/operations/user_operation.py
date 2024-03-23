74af4f28253c4279637f15ca4e2351e6e596d377
maxim <maximbotpy@gmail.com>
2024-03-24 00:14:48 +0300

    убрал дублирующиеся операции

--- Back/src/app/authentication/operations/user_operation.py
+++ Back/src/app/authentication/operations/user_operation.py
@@ -1,51 +1,37 @@
-from datetime import datetime
-from fastapi_users import schemas
+from typing import TYPE_CHECKING
 
-from pydantic import EmailStr
-from sqlalchemy import select, insert, update
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, insert
 
-from core.database import async_session_maker
 from src.app.authentication.models.user import User
-from src.app.authentication.models.news_letter import NewsLetter
+
+if TYPE_CHECKING:
+    from pydantic import EmailStr
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserOperations:
-    @staticmethod
-    async def reset_password(password: str, user_email: EmailStr, session: AsyncSession):
-        stmt = update(User).where(User.email == user_email).values({"password": password})
-        await session.execute(stmt)
-        await session.commit()
 
     @staticmethod
-    async def check_user_exists(log_data: str | EmailStr, session: AsyncSession):
-        match log_data:
-            case log_data if type(log_data) is EmailStr:
+    async def check_user_exists(log_data: str, session: "AsyncSession"):
+        match len(log_data.split("@")):
+            case 2:
                 query = select(User).where(User.email == log_data)
                 execute_query = await session.execute(query)
-
-            case log_data if type(log_data) is str:
+            case 1:
                 query = select(User).where(User.username == log_data)
                 execute_query = await session.execute(query)
+            case _:
+                raise AttributeError("[!] Не удалось получить логин")
 
-        print(execute_query)
-
-    @staticmethod
-    async def subscription_news_letter(user_email: EmailStr, session: AsyncSession):
-        stmt = update(NewsLetter).where(NewsLetter.email == user_email).values({
-            "is_subscription": True,
-            "subscription_date": datetime.utcnow()})
-        await session.execute(stmt)
-        await session.commit()
+        return execute_query.one_or_none()
 
     @staticmethod
-    async def add_role_and_newsletter(email: EmailStr, tables: list):
-        async with async_session_maker() as session:
-            user_id = select(User.id).where(User.email == email)
-            for table in tables:
-                stmt = insert(table).values({"user_id": user_id, "email": email})
-                await session.execute(stmt)
-                await session.commit()
+    async def add_role_and_newsletter(email: "EmailStr", tables: list, session: "AsyncSession"):
+        user = select(User.id).where(User.email == email).scalar_subquery()
+        for table in tables:
+            stmt = insert(table).values({"user_id": user, "email": email})
+            await session.execute(stmt)
+            await session.commit()
 
 
 user = UserOperations()
