79bce71aa3f03ceb36ed1193822efb77faa42fe0
maxim <maximbotpy@gmail.com>
2024-03-24 00:20:41 +0300

    добавил user_role

---
+++ Back/src/app/admin_panel/schemas/admmin.py
@@ -0,0 +1,13 @@
+from uuid import UUID
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+
+class PaginationResponse(BaseModel):
+    id: UUID
+    username: str
+    user_role: str = "user"
+    email: EmailStr
+    time: datetime
+
+    model_config = ConfigDict(from_attributes=True)
