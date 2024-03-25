272acf3e2f08be5954b3c89e85364bc029f1de58
maxim <maximbotpy@gmail.com>
2024-03-25 16:42:25 +0300

    добавил вход по логину/имени, возврат статус кода

--- Back/src/app/authentication/router/api_v1/login.py
+++ Back/src/app/authentication/router/api_v1/login.py
@@ -1,14 +1,18 @@
 from typing import TYPE_CHECKING
-from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from fastapi_users import models
-from fastapi_users.authentication import AuthenticationBackend, Strategy
-from fastapi_users.manager import UserManagerDependency
 from fastapi_users.openapi import OpenAPIResponseType
+from fastapi_users.manager import UserManagerDependency
 from fastapi_users.router.common import ErrorCode, ErrorModel
+from fastapi_users.authentication import AuthenticationBackend, Strategy
+
+from core.schemas.response import Response
+from core.database import get_async_session
 
 if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
     from src.app.authentication.user_manager import UserManager
 
 
@@ -36,9 +40,10 @@ def get_login_user(backend: AuthenticationBackend, get_user_manager: UserManager
     @router.post("/login", name=f"auth:{backend.name}.login", responses=login_responses)
     async def login(credentials: OAuth2PasswordRequestForm = Depends(),
                     user_manager: "UserManager" = Depends(get_user_manager),
-                    strategy: Strategy[models.UP, models.ID] = Depends(backend.get_strategy), ):
+                    strategy: Strategy[models.UP, models.ID] = Depends(backend.get_strategy),
+                    session: "AsyncSession" = Depends(get_async_session)) -> Response:
 
-        user = await user_manager.authenticate(credentials)
+        user = await user_manager.authenticate_custom(credentials, session=session)
 
         if user is None or not user.is_active:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=ErrorCode.LOGIN_BAD_CREDENTIALS, )
@@ -47,6 +52,7 @@ def get_login_user(backend: AuthenticationBackend, get_user_manager: UserManager
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=ErrorCode.LOGIN_USER_NOT_VERIFIED, )
 
         await user_manager.on_after_login(user)
-        return await backend.login(strategy, user)
+        await backend.login(strategy, user)
+        return Response(status_code=status.HTTP_200_OK, data=f"Пользователь вошел в систему")
 
     return router
